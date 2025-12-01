6da6278a84df29fe29eab5ad6e19b214326fe882
aleksei z <farguzalexey@gmail.com>
2025-12-01 16:35:20 +0300

    add webserver to allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'webserver',]
 
 
 # Application definition
