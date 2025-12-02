3ff85d7ff2393f8c14660d9157dbfa99c32633b0
aleksei z <farguzalexey@gmail.com>
2025-12-02 16:12:18 +0300

    add user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,2 +1,12 @@
-
+from django.db import models
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=150)
+    password = models.CharField(max_length=255, validators=[MinLengthValidator(3)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
