5c4c8bf7fd23d334270efac4883bec8d6c0da238
aleksei z <farguzalexey@gmail.com>
2025-12-02 11:10:56 +0300

    add temp comment with the list of future pages

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
-
+'''
+    GET /users/ — страница со списком всех пользователей
+    GET /users/create/ — страница регистрации нового пользователя
+    POST /users/create/ — создание пользователя
+    GET /users/<int:pk>/update/ — страница редактирования пользователя
+    POST /users/<int:pk>/update/ — обновление пользователя
+    GET /users/<int:pk>/delete/ — страница удаления пользователя
+    POST /users/<int:pk>/delete/ — удаление пользователя
+    GET /login/ — страница входа
+    POST /login/ — аутентификация (вход)
+    POST /logout/ — завершение сессии (выход)
+'''
 def index(request):
     return render(
         request,
