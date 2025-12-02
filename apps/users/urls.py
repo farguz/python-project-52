ac09c96c3a78ce2ba2eefdce849767abc85de1c3
aleksei z <farguzalexey@gmail.com>
2025-12-02 15:18:13 +0300

    add create user template and view (nonactionable yet)

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from users import views
+from users.views import RegistrationView, DeleteView, IndexView, UpdateView
 
+# ../users/
 urlpatterns = [
-    path("", views.index),
+    path('', IndexView.as_view()),
+    path('create/', RegistrationView.as_view()),
+    path('<int:user_id>/update/', UpdateView.as_view()),
+    path('<int:user_id>/delete/', DeleteView.as_view()),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.views import View
 
 
 # Create your views here.
-def index(request):
-    return render(
-        request,
-        'users/index.html',
-    )
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'users/index.html',
+        )
+    
+
+class RegistrationView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'users/create.html',
+        )
+    
+
+class UpdateView(View):
+    pass
+    
+
+class DeleteView(View):
+    pass
