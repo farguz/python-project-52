d4d002752a912211c3f419a53c4aca44d156ae8a
aleksei z <farguzalexey@gmail.com>
2025-12-03 11:37:16 +0300

    add reg form

---
+++ apps/users/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from users.models import User
+
+
+class CreateUserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'surname', 'nickname', 'password']
