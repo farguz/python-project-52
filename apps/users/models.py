3c8c684418f21a5b2def502fdeb93d227538f900
aleksei z <farguzalexey@gmail.com>
2025-12-04 16:12:31 +0300

    fix: first_name and last_name is required now

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
-    pass
+    first_name = models.CharField(_("first name"), max_length=150, blank=False)
+    last_name = models.CharField(_("last name"), max_length=150, blank=False)
