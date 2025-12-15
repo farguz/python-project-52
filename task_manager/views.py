6fa2489d08c5f4f0b57da34ff87390bce03098fd
aleksei z <farguzalexey@gmail.com>
2025-12-15 20:12:40 +0300

    fix: login, logout cbv now use default django views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,8 @@
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import redirect, render
-from django.views import View
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render
+from django.urls import reverse_lazy
 
 from .forms import LoginForm
 
@@ -12,40 +14,20 @@ def index(request):
     )
 
 
-class LoginView(View):
-    def get(self, request, *args, **kwargs):
-        form = LoginForm()
-        return render(
-            request,
-            'login.html',
-            context={
-                'form': form,
-                },
-            )
+class TaskManagerLoginView(LoginView):
+    form_class = LoginForm
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        messages.success(self.request, 'Вы успешно вошли в систему!')
+        return reverse_lazy('index_page')
+         
+
+class TaskManagerLogoutView(LoginRequiredMixin, LogoutView):
+
+    next_page = reverse_lazy('index_page')
 
     def post(self, request, *args, **kwargs):
-        form = LoginForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('index_page')
-        else:
-            # prettify later
-            errors = form.errors
-            return render(
-            request,
-            'login.html',
-            context={
-                'form': form,
-                'errors': errors,
-                },
-            )
-        
-
-class LogoutView(View):
-    def post(self, request, *args, **kwargs):
-        logout(request)
-        return redirect('index_page')
+        messages.success(request, 'Вы успешно вышли из профиля!')
+        return super().post(request, *args, **kwargs)
